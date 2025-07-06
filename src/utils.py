3817e9be346de68db34692f7aaec28095893201d
khushali1927 <khushaliagrawal1@gmail.com>
2025-07-06 23:50:56 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -1,64 +1,62 @@
 import os
 import sys
-import numpy as np
-import pandas as pd
-from src.exception import CustomException
-from src.logger import logging
-from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
-import dill  # Using dill for serialization of complex objects
 
-def save_object(obj, file_path):
-    """
-    Save an object to a file using pandas serialization.
-    
-    Parameters:
-    obj (object): The object to be saved.
-    file_path (str): The path where the object will be saved.
-    
-    Returns:
-    None
-    """
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)  # Create directory if it doesn't exist
-        with open(file_path, 'wb') as file:
-            dill.dump(obj, file)  # Use dill to serialize the object
-        logging.info(f"Object saved at {file_path}")
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
-    """
-    Evaluate multiple models and return their performance metrics.
-    
-    Parameters:
-    X (np.ndarray): Feature matrix.
-    y (np.ndarray): Target vector.
-    models (dict): Dictionary of model names and their instances.
-    
-    Returns:
-    dict: Dictionary containing model names and their R2 scores.
-    """
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
+
         for i in range(len(list(models))):
-
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            y_train_pred = model.predict(X_test)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(y_test, y_train_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
 
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
-        
+
         return report
-        
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
